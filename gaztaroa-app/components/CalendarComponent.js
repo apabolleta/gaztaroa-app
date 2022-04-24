d0102b60c4d6febd42f2f3c1a9b9cc591389c61a
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-04-24 19:17:47 +0200

    [NEW] Create Calendar component

---
+++ gaztaroa-app/components/CalendarComponent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ListItem, Avatar } from 'react-native-elements';
+import { SafeAreaView, FlatList } from 'react-native';
+
+function Calendar(props) {
+    const renderCalendarItem = ({item, index}) => {
+        return (
+            <ListItem key={index} bottomDivider>
+                <Avatar source={require('./img/40Años.png')} />
+                <ListItem.Content>
+                    <ListItem.Title>{item.name}</ListItem.Title>
+                    <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    }
+
+    return (
+        <SafeAreaView>
+            <FlatList
+                data={props.trips}
+                renderItem={renderCalendarItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </SafeAreaView>
+    );
+}
+
+export default Calendar;
