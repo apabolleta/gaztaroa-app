45c431972abe2c425b0eae2074aec00c0caa4aa1
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-04-24 20:02:06 +0200

    [EDIT] Allow selection to display details on Calendar component

--- gaztaroa-app/components/BasecampComponent.js
+++ gaztaroa-app/components/BasecampComponent.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import Calendar from './CalendarComponent';
 import { TRIPS } from '../common/trips';
+import TripDetails from './TripDetailsComponent';
+import { View } from 'react-native';
 
 class Basecamp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            trips: TRIPS
+            trips: TRIPS,
+            selectTrip: null
         };
+    }
+
+    onSelectionTrip(tripId) {
+        this.setState({selectTrip: tripId});
     }
 
     render() {
         return (
-            <Calendar trips={this.state.trips} />
+            <View>
+                <TripDetails trip={this.state.trips.filter((trip) => trip.id === this.state.selectTrip)[0]} />
+                <Calendar trips={this.state.trips} onPress={(tripId) => this.onSelectionTrip(tripId)} />
+            </View>
         );
     }
 }
--- gaztaroa-app/components/CalendarComponent.js
+++ gaztaroa-app/components/CalendarComponent.js
@@ -5,7 +5,10 @@ import { SafeAreaView, FlatList } from 'react-native';
 function Calendar(props) {
     const renderCalendarItem = ({item, index}) => {
         return (
-            <ListItem key={index} bottomDivider>
+            <ListItem
+                key={index}
+                onPress={() => props.onPress(item.id)}
+                bottomDivider>
                 <Avatar source={require('./img/40Años.png')} />
                 <ListItem.Content>
                     <ListItem.Title>{item.name}</ListItem.Title>
