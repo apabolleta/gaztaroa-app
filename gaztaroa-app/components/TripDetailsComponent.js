bd637281ed6f3e3c28d7d972f7d1ffa44121f83a
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-04-26 23:23:23 +0200

    [EDIT] Add comments card to trip details screen

--- gaztaroa-app/components/TripDetailsComponent.js
+++ gaztaroa-app/components/TripDetailsComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 import { TRIPS } from '../common/trips';
+import { COMMENTS } from '../common/comments';
 
 function RenderTrip(props) {
     const trip = props.trip;
@@ -24,20 +25,52 @@ function RenderTrip(props) {
     }
 }
 
+function RenderComments(props) {
+    const comments = props.comments;
+
+    const renderCommentItem = ({item, index}) => {
+        return (
+            <View key={index} style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.comment}</Text>
+                <Text style={{fontSize: 12}}>{item.review} Stars</Text>
+                <Text style={{fontSize: 12}}>{'-- ' + item.author + ', ' + item.day} </Text>
+            </View>
+        );
+    };
+
+    return (
+        <Card>
+            <Card.Title>Comentarios</Card.Title>
+            <Card.Divider/>
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+    );
+}
+
 class TripDetails extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            trips: TRIPS
+            trips: TRIPS,
+            comments: COMMENTS
         };
     }
 
     render() {
         const { tripId } = this.props.route.params;
         return (
-            <RenderTrip
-                trip={this.state.trips[+tripId]}
-            />
+            <ScrollView>
+                <RenderTrip
+                    trip={this.state.trips[+tripId]}
+                />
+                <RenderComments
+                    comments={this.state.comments.filter((comment) => comment.tripId === tripId)}
+                />
+            </ScrollView>
         );
     }
 }
