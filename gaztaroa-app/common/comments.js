8b53f088ec3fe49e1db0cebdd07521510aa6b6b9
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-04-26 23:23:15 +0200

    [NEW] Add comments array data

---
+++ gaztaroa-app/common/comments.js
@@ -0,0 +1,163 @@
+export const COMMENTS  =
+[
+    {
+        id : 0,
+        tripId : 0,
+        review : 5,
+        comment : 'Mile esker, Gaztaroa!',
+        author : 'Dani Tiendica',
+        day : '2012-10-16T17 :57 :28.556094Z'
+    },
+    {
+        id : 1,
+        tripId : 0,
+        review : 4,
+        comment : 'Neka neka eginda, baina bueltatuko gara, dudik gabe!',
+        author : 'Aitzi Mendi',
+        day : '2014-09-05T17 :57 :28.556094Z'
+    },
+    {
+        id : 2,
+        tripId : 0,
+        review : 3,
+        comment : 'Hemos disfrutado mucho, aunque al final se hizo un poco largo. Volveremos!',
+        author : 'Marta Huarte',
+        day : '2015-02-13T17 :57 :28.556094Z'
+    },
+    {
+        id : 3,
+        tripId : 0,
+        review : 4,
+        comment : 'Muy buena gente, pero esto no es para mi; gracias por vuestro apoyo!',
+        author : 'Ziriki',
+        day : '2013-12-02T17 :57 :28.556094Z'
+    },
+    {
+        id : 4,
+        tripId : 0,
+        review : 2,
+        comment : 'Estuvo genial! Alguien sabe cuándo es la próxima? Eskerrik asko!',
+        author : 'Manu Colino',
+        day : '2011-12-02T17 :57 :28.556094Z'
+    },
+    {
+        id : 5,
+        tripId : 1,
+        review : 2,
+        comment : 'Estuvo genial! Alguien sabe cuándo es la próxima? Eskerrik asko!',
+        author : 'Manu Colino',
+        day : '2012-10-16T17 :57 :28.556094Z'
+    },
+    {
+        id : 6,
+        tripId : 1,
+        review : 4,
+        comment : 'Muy buena gente, pero esto no es para mi; gracias por vuestro apoyo!',
+        author : 'Ziriki',
+        day : '2014-09-05T17 :57 :28.556094Z'
+    },
+    {
+        id : 7,
+        tripId : 1,
+        review : 3,
+        comment : 'Hemos disfrutado mucho, aunque al final se hizo un poco largo. Volveremos!',
+        author : 'Marta Huarte',
+        day : '2015-02-13T17 :57 :28.556094Z'
+    },
+    {
+        id : 8,
+        tripId : 1,
+        review : 4,
+        comment : 'Neka neka eginda, baina bueltatuko gara, dudik gabe!',
+        author : 'Aitzi Mendi',
+        day : '2013-12-02T17 :57 :28.556094Z'
+    },
+    {
+        id : 9,
+        tripId : 1,
+        review : 2,
+        comment : 'Mile esker, Gaztaroa!',
+        author : 'Dani Tiendica',
+        day : '2011-12-02T17 :57 :28.556094Z'
+    },
+    {
+        id : 10,
+        tripId : 2,
+        review : 5,
+        comment : 'Neka neka eginda, baina bueltatuko gara, dudik gabe!',
+        author : 'Aitzi Mendi',
+        day : '2012-10-16T17 :57 :28.556094Z'
+    },
+    {
+        id : 11,
+        tripId : 2,
+        review : 4,
+        comment : 'Hemos disfrutado mucho, aunque al final se hizo un poco largo. Volveremos!',
+        author : 'Marta Huarte',
+        day : '2014-09-05T17 :57 :28.556094Z'
+    },
+    {
+        id : 12,
+        tripId : 2,
+        review : 3,
+        comment : 'Mile esker, Gaztaroa!',
+        author : 'Dani Tiendica',
+        day : '2015-02-13T17 :57 :28.556094Z'
+    },
+    {
+        id : 13,
+        tripId : 2,
+        review : 4,
+        comment : 'Estuvo genial! Alguien sabe cuándo es la próxima? Eskerrik asko!',
+        author : 'Manu Colino',
+        day : '2013-12-02T17 :57 :28.556094Z'
+    },
+    {
+        id : 14,
+        tripId : 2,
+        review : 2,
+        comment : 'Muy buena gente, pero esto no es para mi; gracias por vuestro apoyo!',
+        author : 'Ziriki',
+        day : '2011-12-02T17 :57 :28.556094Z'
+    },
+    {
+        id : 15,
+        tripId : 3,
+        review : 5,
+        comment : 'Mile esker, Gaztaroa!',
+        author : 'Dani Tiendica',
+        day : '2012-10-16T17 :57 :28.556094Z'
+    },
+    {
+        id : 16,
+        tripId : 3,
+        review : 4,
+        comment : 'Estuvo genial! Alguien sabe cuándo es la próxima? Eskerrik asko!',
+        author : 'Manu Colino',
+        day : '2014-09-05T17 :57 :28.556094Z'
+    },
+    {
+        id : 17,
+        tripId : 3,
+        review : 3,
+        comment : 'Muy buena gente, pero esto no es para mi; gracias por vuestro apoyo!',
+        author : 'Ziriki',
+        day : '2015-02-13T17 :57 :28.556094Z'
+    },
+    {
+        id : 18,
+        tripId : 3,
+        review : 4,
+        comment : 'Hemos disfrutado mucho, aunque al final se hizo un poco largo. Volveremos!',
+        author : 'Marta Huarte',
+        day : '2013-12-02T17 :57 :28.556094Z'
+    },
+    {
+        id : 19,
+        tripId : 3,
+        review : 2,
+        comment : 'Nada que objetar ni comentar...',
+        author : 'Anónimo',
+        day : '2011-12-02T17 :57 :28.556094Z'
+    }
+];
