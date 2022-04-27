25c5136bdcd1868cfc16b2f53b707f3dfa3d93cb
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-04-27 20:12:20 +0200

    [DEL] Remove local data containers

--- gaztaroa-app/common/headers.js
+++
@@ -1,9 +0,0 @@
-export const HEADERS = [
-    {
-        id: 0,
-        name: '40 años de historia!',
-        img: 'img/mendiak.png',
-        featured: true,
-        description: '40 años organizando salidas para conocer y disfrutar la montaña'
-    }
-];
